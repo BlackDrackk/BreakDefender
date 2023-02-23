0b5483bd421f18904b5cd0ce818e3a37f9fb0341
owen62 <65536127+owen62@users.noreply.github.com>
2023-02-23 14:08:50 +0100

    learning c++ P1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
+/*
+Directives de préprocesseurs, le préprocesseur est un programme qui va parser les fichiers (parcourir les fichiers) et à chaque instruction/directives il va executer l'ordre qu'on lui donne ici "include ..."" 
+*/
+
+#include <iostream>
 
 /*
 Fonction principale "main"
 Tous les programmes commencent par la fonction main
 */
+
+
 int main()
 {
+    int age = 0;
+    std::cout << "How old are you ?";
+    std::cin >> age;
+    std::cout << "I am : " << age;
+
     return 0;
 }
 
